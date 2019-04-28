fbafe4d4ea3ed267ff7cdbe5400bd5776bd0fa7c
Sergio E. Restrepo R <sergiores24@gmail.com>
2019-04-27 21:06:58 -0500

    Task is created and added to it's group

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,5 +1,6 @@
 var Task 	= require('../models/task');
-var User	= require('../models/user');
+var User 	= require('../models/user');
+var TasksGroup=require('../models/tasksgroup');
 var {body,validationResult} = require('express-validator/check');
 
 exports.validator=(method)=>{
@@ -9,6 +10,7 @@ exports.validator=(method)=>{
 				body('name','Not name provided').exists().isString(),
 				body('activities','Not an array').optional().isArray(),
 				body('users','Not an array').optional().isArray(),
+				body('groupId','No Tasks group ID provided').exists()
 			]
 		}
 	}
@@ -18,36 +20,48 @@ exports.createTask=(req,res)=>{
 	const errors = validationResult(req);
 	if(!errors.isEmpty()){return res.status(500).json({errors: errors.array()});}
 
-	if(!req.body.users){
-		var taskModel=Task({
-				name: req.body.name,
-				status: 'Open',
-				activities: req.body.activities
-		});
-		taskModel.save((err,task)=>{
-			if(err) return res.status(500).send('Task could not be created');
-			return res.json(task);
-		});
-	}
-	else{
-		var ids =req.body.users;
-		var length = ids.length;
-		User.find({_id:{$in:ids}},(err,users)=>{
-			if(err) return res.status(500).send('Users could not be found');
-			if(!users) return res.status(404).send('Users not found');
-			if(users.length!=length) return res.status(404).send('One or more users not found');
+	TasksGroup.findById(req.body.groupId,(err,tgroup)=>{
+		if(err) return res.status(500).send('could not find the Tasks Group');
+		if(!tgroup) return res.status(404).send('Tasks Group not found');
+		if(!req.body.users){
 			var taskModel=Task({
-				name: req.body.name,
-				status: 'Open',
-				activities: req.body.activities,
-				users: req.body.users
+					name: req.body.name,
+					status: 'Open',
+					activities: req.body.activities
 			});
 			taskModel.save((err,task)=>{
 				if(err) return res.status(500).send('Task could not be created');
-				return res.json(task);
+				tgroup.tasks.push(task._id);
+				tgroup.save((err,tg)=>{
+					if(err) res.status(500).json(err);
+					return res.send('Task created and added to It\'s group')
+				});
 			});
-		});
-	}
+		}
+		else{
+			var ids =req.body.users;
+			var length = ids.length;
+			User.find({_id:{$in:ids}},(err,users)=>{
+				if(err) return res.status(500).send('Users could not be found');
+				if(!users) return res.status(404).send('Users not found');
+				if(users.length!=length) return res.status(404).send('One or more users not found');
+				var taskModel=Task({
+					name: req.body.name,
+					status: 'Open',
+					activities: req.body.activities,
+					users: req.body.users
+				});
+				taskModel.save((err,task)=>{
+					if(err) return res.status(500).send('Task could not be created');
+					tgroup.tasks.push(task._id);
+					tgroup.save((err,tg)=>{
+						if(err) res.status(500).json(err);
+						return res.send('Task created and added to It\'s group')
+					});
+				});
+			});
+		}
+	});
 }
 
 exports.getTaskUsers=(req,res)=>{
